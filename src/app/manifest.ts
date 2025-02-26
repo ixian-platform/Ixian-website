f91f784f79d5541dbf31ce572abbbf4eca04d1bc
jakapozun <jaka.pozun2@gmail.com>
2025-02-26 13:30:26 +0100

    fix: website icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,24 +3,26 @@ import type { MetadataRoute } from 'next';
 export const dynamic = 'force-static';
 
 export default function manifest(): MetadataRoute.Manifest {
+  const siteUrl = process.env.SITE_URL || 'https://www.ixian.io';
+
   return {
     name: 'Ixian',
     short_name: 'Ixian',
     description:
       'Ixian is an open-source decentralized data streaming platform designed for seamless, secure, and scalable connectivity. Reduce costs, enhance security, and effortlessly integrate with IoT, smart cities, and real-time applications.',
-    start_url: '/',
+    start_url: `${siteUrl}` || '/',
     display: 'standalone',
     background_color: '#fff',
     theme_color: '#fff',
     icons: [
       {
-        src: '/web-app-manifest-192x192.png',
+        src: `${siteUrl}/web-app-manifest-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/web-app-manifest-512x512.png',
+        src: `${siteUrl}/web-app-manifest-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
